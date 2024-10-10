38422ef8f5256943bb467da16d83b34052f094e5
A612\1DAW <davalorib@gmail.com>
2024-10-10 21:18:46 +0200

    subo avances de bateria tema 2

--- Tema2/src/main/java/org/example/Bateria.java
+++ Tema2/src/main/java/org/example/Bateria.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Bateria {
 
     public void bateria_1(){
 
-        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce una frase a eleccción: ");
+
+        String frase = entrada.nextLine();
+
+        System.out.println("Tu frase: " + frase);
+        System.out.println("");
+
+        int tamanyo = frase.length();
+        System.out.println("El tamaño de la frase es " + tamanyo);
 
 
     }
-
-
 }
